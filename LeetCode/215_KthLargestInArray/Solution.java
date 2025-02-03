d3ee6221357b3525443b77bdf84a6530bede2339
pranjallsharmaa <51287268+pranjallsharmaa@users.noreply.github.com>
2025-02-03 22:39:22 +0530

    Update Solution.java for #215

--- LeetCode/215_KthLargestInArray/Solution.java
+++ LeetCode/215_KthLargestInArray/Solution.java
@@ -1,10 +1,14 @@
 class Solution {
     public int findKthLargest(int[] nums, int k) {
         PriorityQueue<Integer> minHeap = new PriorityQueue<>();
-        for(int i = 0; i < nums.length; i++) {
+         for (int i = 0; i < k; i++) {
             minHeap.add(nums[i]);
-            if(minHeap.size() > k) {
+        }
+        
+        for (int i = k; i < nums.length; i++) {
+            if (nums[i] > minHeap.peek()) {
                 minHeap.poll();
+                minHeap.add(nums[i]);
             }
         }
         return minHeap.peek();
